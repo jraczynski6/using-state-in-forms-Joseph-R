645bc47baaeb3613f390182a59556f411e164d79
jraczynski <raczynski.joseph616@gmail.com$ git config --global user.email raczynski.joseph616@gmail.com>
2025-10-23 19:07:32 -0500

    add pair work code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import UserForm from './components/FormData'
+import UserForm from './components/FeedbackForm'
 import './App.css'
 function App() {
   return (
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+
 export default function UserForm() {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
-        feedBack: "",
+        feedback: "",
     });
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData((prevData) => ({
@@ -12,17 +14,38 @@ export default function UserForm() {
             [name]: value,
         }))
     }
+
+    const isDisabled = !formData.name.trim() || !formData.email.trim() || !formData.feedback.trim();
+
     return (
-        <div>
-            <h1>User-Form</h1>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
-                </label>
-            </form>
-            <h2>Preview</h2>
-            <p>Name:{formData.name}</p>
+        <div className="page-container">
+            <div className="form-container">
+                <form>
+                    <fieldset>
+                        <legend>User Form</legend>
+                        <label>
+                            Name:
+                            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Email:
+                            <input type="text" name="email" value={formData.email} onChange={handleChange} />
+                        </label>
+                        <label>
+                            Feedback:
+                            <textarea name="feedback" value={formData.feedback} placeholder="Please write your feedback here." onChange={handleChange} maxLength="200" />
+                        </label>
+                        <br/>
+                        <button type="submit" disabled={isDisabled}>Submit Form</button>
+                    </fieldset>
+                </form>
+            </div>
+            <div className="preview-container">
+                <h2>Preview</h2>
+                <p>Name: {formData.name}</p>
+                <p>Email: {formData.email}</p>
+                <p>Feedback: {formData.feedback}</p>
+            </div>
         </div>
     )
 }
